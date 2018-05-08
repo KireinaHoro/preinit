18379aae83b32938c270b30bb956c732f5b461b0
KireinaHoro <i@jsteward.moe>
2018-05-09 01:27:52 +0800

    demo init that utilizes log_facility

--- src/init.cc
+++ src/init.cc
@@ -1,1 +1,19 @@
+// init.cc: entry point from initramfs.
+//
+// 2018 KireinaHoro <i@jsteward.moe>
+
 #include "util/log_facility.h"
+
+int main() {
+  SLOGD("init started.");
+  
+  SLOGI("Old pond");
+  SLOGI("Frog jumps in");
+  SLOGI("Sound of water");
+  SLOGI("-- Matsuo Basho");
+
+  SLOGW("This is the end of the demo init.");
+
+  SLOGE("End of init reached -- kernel panic will now occur");
+  return 0;
+}
