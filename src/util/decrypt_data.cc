e2863b325c6a76c33fe61d2b26c76917a4f2b5b7
KireinaHoro <i@jsteward.moe>
2018-05-08 14:41:15 +0800

    skel

---
+++ src/util/decrypt_data.cc
@@ -0,0 +1,6 @@
+// decrypt_data.cc: utilities for decrypting the /data partition
+// on Full-Disk Encryption devices.
+//
+// 2018 KireinaHoro <i@jsteward.moe>
+
+#include "decrypt_data.h"
---
+++ src/util/decrypt_data.h
@@ -0,0 +1,9 @@
+// decrypt_data.h: utilities for decrypting the /data partition
+// on Full-Disk Encryption devices.
+//
+// 2018 KireinaHoro <i@jsteward.moe>
+
+#ifndef DECRYPT_DATA_H
+#define DECRYPT_DATA_H
+
+#endif
