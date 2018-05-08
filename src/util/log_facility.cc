69f2bb8af34c19bfd78458f39d4e9d98772a1b55
KireinaHoro <i@jsteward.moe>
2018-05-08 23:37:31 +0800

    add log facility implementation

--- src/util/log_facility.cc
+++ src/util/log_facility.cc
@@ -1,5 +1,73 @@
-// log_facility.h: log facility using /dev/kmsg, avaliable for reading on next boot
+// log_facility.cc: log facility using /dev/kmsg, avaliable for reading on next boot
 //
 // 2018 KireinaHoro <i@jsteward.moe>
 
+#include <cstring>
+#include <fstream>
+#include <unistd.h>
+#include <sys/mount.h>
+
 #include "log_facility.h"
+
+// Note that before we set up the log device properly, there will be no way
+// to store the log even if an error happens. We're making rough assumptions
+// and not checking for other possible errors here.
+
+namespace klog {
+  char *process_message(log_level lvl, const char *message) {
+    const char *log_level_string = log_level_strings[lvl];
+
+    auto total_len = std::strlen(log_prefix) + std::strlen(log_level_string) + std::strlen(message) + 1;
+    auto ret_buffer = new char[total_len];
+    std::strcpy(ret_buffer, log_prefix);
+    std::strcpy(ret_buffer, log_level_string);
+    std::strcpy(ret_buffer, message);
+
+    return ret_buffer;
+  }
+
+  bool check_log_facility() {
+    // if the stat syscall succeeded, /dev/kmsg exists
+    // we can assume that a working /dev has been set up
+    if (setup_finished) { return true; }
+    struct stat buffer;
+    setup_finished = (stat(kmsg_path, &buffer) == 0);
+    return setup_finished;
+  }
+
+  bool setup_log_facility() {
+    // check if things are already set up
+    if (check_log_facility()) {
+      return true;
+    }
+
+    // mount devtmpfs at /dev
+    // check mountpoint
+    struct stat buffer;
+    if (stat("/dev", &buffer) != 0) {
+      // mountpoint doesn't exist
+      mkdir("/dev", S_IRWXU);
+    } else if (!S_ISDIR(buffer.st_mode)) {
+      // something's in the way -- unlink it
+      unlink("/dev");
+      mkdir("/dev", S_IRWXU);
+    }
+    // mount devtmpfs
+    mount("none", "/dev", "devtmpfs", "");
+
+    return check_log_facility();
+  }
+
+  void log_to_facility_with_level(log_level lvl, const char *message) {
+    if (!setup_log_facility()) {
+      // the log facility failed to get ready -- return
+      return;
+    }
+    std::ofstream out(kmsg_path);
+    auto *buffer = process_message(message);
+    out << buffer;
+    delete buffer;
+  }
+}
+    
+      
---
+++ src/util/log_facility.h
@@ -0,0 +1,43 @@
+// log_facility.h: log facility using /dev/kmsg, avaliable for reading on next boot
+//
+// 2018 KireinaHoro <i@jsteward.moe>
+
+#ifndef LOG_FACILITY_H
+#define LOG_FACILITY_H
+
+namespace klog {
+  bool setup_finished = false;
+  
+  const char *kmsg_path = "/dev/kmsg";
+  const char *log_prefix = "new_era ";
+  enum log_level {
+    LOG_DEBUG,
+    LOG_INFO,
+    LOG_WARN,
+    LOG_FATAL,
+  };
+  const char (*log_level_strings)[] = {
+    "[DEBUG] ",
+    "[INFO ] ",
+    "[WARN ] ",
+    "[FATAL] ",
+  };
+
+  // concatencate the log message into string that will be written to kmsg
+  // remember to free the pointer after use to avoid leakage
+  char *process_message(log_level lvl, const char *message);
+
+  // check if /dev/kmsg is available for use
+  bool check_log_facility();
+  // check and setup /dev/kmsg
+  bool setup_log_facility();
+
+  void log_to_facility_with_level(log_level lvl, const char *message)
+}
+
+#define SLOGD(msg) klog::log_to_facility_with_level(LOG_DEBUG, msg)
+#define SLOGI(msg) klog::log_to_facility_with_level(LOG_INFO, msg)
+#define SLOGW(msg) klog::log_to_facility_with_level(LOG_WARN, msg)
+#define SLOGE(msg) klog::log_to_facility_with_level(LOG_FATAL, msg)
+
+#endif
